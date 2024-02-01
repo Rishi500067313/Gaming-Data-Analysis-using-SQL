ea4c2f86d53f4d715ce4d85a22162026966df20e
Rishi pandey <50805925+Rishi500067313@users.noreply.github.com>
2024-02-01 16:41:20 +0530

    Update Allqueries.sql

--- Allqueries.sql
+++ Allqueries.sql
@@ -5,7 +5,7 @@ SELECT user_key, type, date, count(1) as Num_duplicate_records
     group by p.date, p.type, p.user_key
     having count(1) > 1;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #Query to get actual duplicates
 with dup_record as (
     select user_key, type, date, 
@@ -15,7 +15,7 @@ with dup_record as (
 )
 select user_key, type, date from dup_record where row_num > 1;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #Query to get distinct user who are not loggedin Approach 1
 select count(distinct p.user_key) as Num_of_user
 from ea_test.prep_user p
@@ -23,7 +23,7 @@ left join ea_test.dim_user d on p.user_key = d.user_key
 left join ea_test.user_login u on u.user_id = d.user_id
 where p.type in ('purchase') and u.user_id is null;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #Query to get distinct user who are not loggedin Approach 2
 select count(distinct p.user_key) as Num_of_user
 from ea_test.prep_user p
@@ -31,7 +31,7 @@ left join ea_test.dim_user d on p.user_key = d.user_key
 where d.user_id not in (select user_id from ea_test.user_login) and p.type in ('purchase');
 
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #Query to check records inserted for each day during backfills
 select Date, count(*) as Record_Num 
 from ea_test.prep_user 
@@ -39,7 +39,7 @@ where date between '2022-08-01' and '2022-08-05'
 group by date 
 order by date;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #Query to identify date mismatches between prep_user & user_login.
 with pre_work as (
     select pr.user_key, di.user_id, date,
@@ -55,15 +55,15 @@ from ea_test.user_login us
       join pre_work p_w on p_w.user_id = us.user_id and p_w.row_num = 1
 where us.first_login_date != p_w.date;
 
-
-#Query to find users who didn't made purchase
+---------------------------------------------------------------------------------------------------------------------------
+#Query to find users who did not made purchase
 select di.user_key
 from ea_test.dim_user di
      left join ea_test.prep_user pr on pr.date = di.date_id
 where
     pr.type not in ('purchase') and di.user_key like '-%';
 
-
+---------------------------------------------------------------------------------------------------------------------------
 # query using windoww function
 with sec_score as (
     select user_id, platform, date, Score, 
@@ -77,7 +77,7 @@ Select user_id, platform, date, Score
 from sec_score 
 where row_num = 2;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #query without using window function
 with sec_score as (
     select ps1.user_id, ps1.platform, ps1.date, max(ps1.Score) as score
@@ -92,7 +92,7 @@ join (select date, platform, max(score) as score
 	  from sec_score 
       group by date, platform) a on a.date= c.date and a.platform = c.platform and a.score = c.score;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #query to get raw_events in shape
 with pre_process as(
 select
@@ -103,7 +103,7 @@ from ea_test.raw_events_1
 )
 select platform, title, count(distinct(user_id)) from pre_process group by title, platform;
 
-
+---------------------------------------------------------------------------------------------------------------------------
 #query for valid json & seperation
 with json_val as (
     select
